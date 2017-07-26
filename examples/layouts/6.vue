cc3f34ac6fb766c80ae9c48c1eef130fcedf51df
John Leider <john.j.leider@gmail.com>
2017-07-25 20:52:52 -0400

    fixed markup

--- examples/layouts/6.vue
+++ examples/layouts/6.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app id="example-2" standalone>
-    <v-navigation-drawer persistent light :mini-variant.sync="mini" v-model="drawer" overflow>
+    <v-navigation-drawer absolute persistent light :mini-variant.sync="mini" v-model="drawer" overflow>
       <v-toolbar flat class="transparent">
         <v-list class="pa-0">
           <v-list-tile avatar tag="div">
