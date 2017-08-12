553d553c08b2e847700bd1335f398ab646cee47b
John Leider <john.j.leider@gmail.com>
2017-08-12 16:45:14 -0400

    resolves #158

--- pages/TransitionsView.vue
+++ pages/TransitionsView.vue
@@ -27,7 +27,7 @@
       return {
         doc: {
           title: 'Motion',
-          edit: 'Transitions',
+          edit: 'TransitionsView',
           desc: `Smooth animations help make a UI feel great. Using Vue's transition system and re-usable functional components, you can easily control the motion of your application. Most components can have their transition altered through the <code>transition</code> prop.`,
           examples: [
             { header: "Slide X transitions", file: 'transitions/1', desc: 'Slide x transitions move along the horizontal axis.' },
