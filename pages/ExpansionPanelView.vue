f8a01ce482d3783ab784818fb4eea6d0f3810882
Albert Kaaman <albert.kaaman@axis.com>
2017-08-23 13:18:10 +0200

    added inset, popout, and focusable expansion panel examples

--- pages/ExpansionPanelView.vue
+++ pages/ExpansionPanelView.vue
@@ -13,13 +13,33 @@
           desc: `The <code>v-expansion-panel</code> component is useful for reducing vertical space with large amounts of information. The default functionality of the component is to only display one expansion-panel body at a time, however, with the <code>expandable</code> property, the expansion-panel can remain open until explicity closed.`,
           examples: [
             { header: 'Accordion', file: 'expansion-panel/1', desc: 'Accordion expansion panels can only have 1 panel open at a time.' },
-            { header: 'Expand', file: 'expansion-panel/2', desc: 'Expand expansion panels will stay open until closed.' }
+            { header: 'Expand', file: 'expansion-panel/2', desc: 'Expand expansion panels will stay open until closed.' },
+            { header: 'Popout & Inset', file: 'expansion-panel/3', desc: 'The expansion panel also has two alternative designs you can activate with the props <code>popout</code> and <code>inset</code>.' },
+            { header: 'Focusable', file: 'expansion-panel/4', desc: 'The expansion panel headers can be made focusable with the prop <code>focusable</code>.' }
           ],
           props: {
             'v-expansion-panel': {
               params: [
                 [
                   'expand',
+                  'Boolean',
+                  'False',
+                  'Leaves expansion-panel open when selecting another'
+                ],
+                [
+                  'focusable',
+                  'Boolean',
+                  'False',
+                  'Leaves expansion-panel open when selecting another'
+                ],
+                [
+                  'inset',
+                  'Boolean',
+                  'False',
+                  'Leaves expansion-panel open when selecting another'
+                ],
+                [
+                  'popout',
                   'Boolean',
                   'False',
                   'Leaves expansion-panel open when selecting another'
