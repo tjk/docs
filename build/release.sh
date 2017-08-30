1f973facaf62ecae3d5a4f2519763eeec597c6dc
John Leider <john.j.leider@gmail.com>
2017-08-29 20:42:37 -0400

    updating archive/release process

--- build/release.sh
+++ build/release.sh
@@ -1,7 +1,35 @@
-echo "Building"
+#!/bin/bash
 
-npm run build
-git add -A
-git commit -m "[build]"
+set -e
+# Set archive version for current docs
+echo "Enter archive version: "
+read RELEASE
 
-git push
+# Set release version for new docs
+echo "Enter release version: "
+read NEXT
+
+# Remove any local bindings
+yarn unlink vuetify
+
+# Refresh package installation
+git checkout master
+git pull
+rm -rf node_modules
+yarn
+
+# Run archive
+RELEASE=$RELEASE NEXT=$NEXT npm run archive
+
+# Move to master
+git add .
+git stash
+git co dev
+git stash pop
+git merge master
+
+# npm run build
+# git add -A
+# git commit -m "[build]"
+
+# git push
