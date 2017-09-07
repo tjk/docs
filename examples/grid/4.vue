ac6102792f2da2392e8a887c355f4b200b17c506
John Leider <john.j.leider@gmail.com>
2017-09-07 12:42:13 -0400

    improved grid example

--- examples/grid/4.vue
+++ examples/grid/4.vue
@@ -34,5 +34,22 @@
         </v-card>
       </v-flex>
     </v-layout>
+    <v-layout row justify-center>
+      <v-flex xs2>
+        <v-card dark class="primary">
+          <v-card-text>one</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex xs2>
+        <v-card dark class="secondary">
+          <v-card-text>two</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex xs2>
+        <v-card dark class="accent">
+          <v-card-text>three</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
