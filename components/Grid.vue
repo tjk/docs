dfd27ebcc4f0ed70e1b45d1719944196369b0618
nekosaur <albert@kaaman.nu>
2017-09-10 19:59:43 +0200

    fixed breakpoint grid table

--- components/Grid.vue
+++ components/Grid.vue
@@ -7,7 +7,7 @@
         thead
           tr.text-xs-left
             th Device
-            th strong Code
+            th Code
             th Types
             th Range
         tbody
@@ -15,40 +15,45 @@
             td
               v-icon.mr-3 phone_iphone
               span Extra small
-            td strong xs
+            td
+              strong xs
             td small to large handset
             td &lt; 600px
           tr
             td
               v-icon.mr-3 tablet
               span Small
-            td strong sm
+            td
+              strong sm
             td small to medium tablet
             td 600px &gt; &lt; 960px
           tr
             td
               v-icon.mr-3 laptop
               span Medium
-            td strong md
+            td
+              strong md
             td large tablet to laptop
             td 960px &gt; &lt; 1264*
           tr
             td
               v-icon.mr-3 desktop_windows
               span Large
-            td strong lg
+            td
+              strong lg
             td desktop
             td 1264 &gt; &lt; 1904px*
           tr
             td
               v-icon.mr-3 tv
               span Extra large
-            td strong xl
+            td
+              strong xl
             td 4k and ultra-wides
             td &gt; 1904px*
         tfoot
           tr
-            td(colspan="3").text-xs-center
+            td(colspan="4").text-xs-center
               small
                 em.grey--text * -16px on Desktop
 </template>
