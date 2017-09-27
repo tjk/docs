f2682e01ea6c1fce9e4107b9636930f3e0b0ef6e
Pedro Luiz Prado Vieira <peluprvi@gmail.com>
2017-09-26 21:39:58 -0300

    Rename parallax example files

--- pages/ParallaxView.vue
+++ pages/ParallaxView.vue
@@ -14,10 +14,10 @@
           title: 'Parallax',
           desc: 'The <code>v-parallax</code> component creates a 3d effect that makes an image appear to scroll slower than the window.',
           examples: [
-            { header: 'Default', file: 'parallax/1', desc: 'A parallax causes a shift in a background image when the user scrolls the page.' },
-            { header: 'With content', file: 'parallax/2', desc: 'You can also place any content inside of the parallax. This allows you to use the parallax as a hero image.' },
-            { header: 'Custom height', file: 'parallax/3', desc: 'You can specify a custom height on a parallax. Keep in mind this can break the parallax if your image is not sized properly' },
-            { header: 'Jumbotron', file: 'parallax/4', new: '0.15.3', desc: 'The parallax can have its effect disabled to be used as a standard <strong>jumbotron</strong>' }
+            { header: 'Default', file: 'parallax/default', desc: 'A parallax causes a shift in a background image when the user scrolls the page.' },
+            { header: 'With content', file: 'parallax/content', desc: 'You can also place any content inside of the parallax. This allows you to use the parallax as a hero image.' },
+            { header: 'Custom height', file: 'parallax/custom-height', desc: 'You can specify a custom height on a parallax. Keep in mind this can break the parallax if your image is not sized properly' },
+            { header: 'Jumbotron', file: 'parallax/jumbotron', new: '0.15.3', desc: 'The parallax can have its effect disabled to be used as a standard <strong>jumbotron</strong>' }
           ],
           props: {
             'v-parallax': {
