ba570fc536d3fca7d2b4540ebf86e4ab78d14d48
Kael <kaelwd@gmail.com>
2017-09-29 18:43:49 +1000

    Correct indentation in generated codepens

--- examples/forms/validation-with-submit-and-clear.vue
+++ examples/forms/validation-with-submit-and-clear.vue
@@ -39,29 +39,27 @@
 
 <script>
   export default {
-    data () {
-      return {
-        valid: true,
-        name: '',
-        nameRules: [
-          (v) => !!v || 'Name is required',
-          (v) => v && v.length <= 10 || 'Name must be less than 10 characters'
-        ],
-        email: '',
-        emailRules: [
-          (v) => !!v || 'E-mail is required',
-          (v) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
-        ],
-        select: null,
-        items: [
-          'Item 1',
-          'Item 2',
-          'Item 3',
-          'Item 4'
-        ],
-        checkbox: false
-      }
-    },
+    data: () => ({
+      valid: true,
+      name: '',
+      nameRules: [
+        (v) => !!v || 'Name is required',
+        (v) => v && v.length <= 10 || 'Name must be less than 10 characters'
+      ],
+      email: '',
+      emailRules: [
+        (v) => !!v || 'E-mail is required',
+        (v) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+      ],
+      select: null,
+      items: [
+        'Item 1',
+        'Item 2',
+        'Item 3',
+        'Item 4'
+      ],
+      checkbox: false
+    }),
     methods: {
       submit () {
         if (this.$refs.form.validate()) {
