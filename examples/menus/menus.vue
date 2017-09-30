35c3038ff8d4c11828571812ddd9a43e32fd2bce
Pedro Vieira <peluprvi@gmail.com>
2017-09-30 08:26:41 -0300

    Changed menu position

--- examples/menus/menus.vue
+++ examples/menus/menus.vue
@@ -5,7 +5,7 @@
         <v-card-title class="blue white--text">
           <span class="headline">Menu</span>
           <v-spacer></v-spacer>
-          <v-menu bottom right>
+          <v-menu bottom left>
             <v-btn icon slot="activator" dark>
               <v-icon>more_vert</v-icon>
             </v-btn>
