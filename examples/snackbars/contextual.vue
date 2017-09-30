da634bcf81e1ac92202de70b41671700dfd24c59
Jacek Karczmarczyk <jkarczm@gmail.com>
2017-09-30 14:39:56 +0700

    colorable snacks

--- examples/snackbars/contextual.vue
+++ examples/snackbars/contextual.vue
@@ -4,13 +4,14 @@
       <v-container fluid>
         <v-layout row wrap>
           <v-flex xs12>
-            <v-radio-group v-model="context" row>
-              <v-radio value="success" label="Success" light class="success--text"></v-radio>
-              <v-radio value="info" label="Info" light class="info--text"></v-radio>
-              <v-radio value="warning" label="Warning" light class="warning--text"></v-radio>
-              <v-radio value="error" label="Error" light class="error--text"></v-radio>
-              <v-radio value="primary" label="Primary" light class="primary--text"></v-radio>
-              <v-radio value="secondary" label="Secondary" light class="secondary--text"></v-radio>
+            <v-radio-group v-model="color" row>
+              <v-radio 
+                v-for=" colorValue in ['success', 'info', 'error', 'cyan darken-2']" 
+                light 
+                :value="colorValue" 
+                :label="colorValue" 
+                :color="colorValue"
+              ></v-radio>
             </v-radio-group>
           </v-flex>
           <v-flex xs12 sm3>
@@ -32,12 +33,7 @@
     </v-card-text>
     <v-snackbar
       :timeout="timeout"
-      :success="context === 'success'"
-      :info="context === 'info'"
-      :warning="context === 'warning'"
-      :error="context === 'error'"
-      :primary="context === 'primary'"
-      :secondary="context === 'secondary'"
+      :color="color"
       :multi-line="mode === 'multi-line'"
       :vertical="mode === 'vertical'"
       v-model="snackbar"
@@ -53,7 +49,7 @@
     data () {
       return {
         snackbar: false,
-        context: '',
+        color: '',
         mode: '',
         timeout: 6000,
         text: 'Hello, I\'m a snackbar'
