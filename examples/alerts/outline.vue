645365d5f42362ce60f7de17a087d23520e1e74e
Hendrik Grobler <hendrikg@miway.co.za>
2017-10-30 18:15:12 +0200

    Add example for alert component's outline prop

---
+++ examples/alerts/outline.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-alert outline="true" color="success" icon="check_circle" value="true">
+      This is a success alert.
+    </v-alert>
+
+    <v-alert outline="true" color="info" icon="info" value="true">
+      This is an info alert.
+    </v-alert>
+
+    <v-alert outline="true" color="warning" icon="priority_high" value="true">
+      This is a warning alert.
+    </v-alert>
+
+    <v-alert outline="true" color="error" icon="warning" value="true">
+      This is a error alert.
+    </v-alert>
+  </div>
+</template>
