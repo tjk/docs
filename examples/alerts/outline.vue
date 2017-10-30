da45f4e3747a00fef79fb971d37011f1352203f8
Hendrik Grobler <hendrikg@miway.co.za>
2017-10-30 19:42:03 +0200

    Update outline and value props on example to be Boolean instead of string

--- examples/alerts/outline.vue
+++ examples/alerts/outline.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
-    <v-alert outline="true" color="success" icon="check_circle" value="true">
+    <v-alert outline color="success" icon="check_circle" :value="true">
       This is a success alert.
     </v-alert>
 
-    <v-alert outline="true" color="info" icon="info" value="true">
+    <v-alert outline color="info" icon="info" :value="true">
       This is an info alert.
     </v-alert>
 
-    <v-alert outline="true" color="warning" icon="priority_high" value="true">
+    <v-alert outline color="warning" icon="priority_high" :value="true">
       This is a warning alert.
     </v-alert>
 
-    <v-alert outline="true" color="error" icon="warning" value="true">
+    <v-alert outline color="error" icon="warning" :value="true">
       This is a error alert.
     </v-alert>
   </div>
